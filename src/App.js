50ea38e70b8d3e29fd35357b24e018e242bd70c1
Pamela Jung <72463342+pamify@users.noreply.github.com>
2022-03-23 01:05:02 -0400

    implement useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
+import { useState } from "react";
+
 import "./App.css";
 import { familyMadrigal, mirabelMadrigal } from "./familyMadrigal.js";
 
 function App() {
+  // the value passed into useState is the default value
+  /* madrigal = {
+      name: "Mirabel",
+      description:
+        "Hi! I'm Mirabel. Today's a super special day for us - The Family Madrigal. Come learn about my family!",
+      image: "https://upload.wikimedia.org/wikipedia/en/e/e7/Mirabel_Madrigal.png",
+      alt: "This is me - Mirabel Madrigal"
+  } */
+
+  /* on line 33, madrigal.description is "Hi! I'm Mirabel. Today's a super special day for us - The Family Madrigal. Come learn about my family!" */
+
+  const [madrigal, setMadrigal] = useState(mirabelMadrigal);
+  const [weDontTalkAboutBruno, setWeDontTalkAboutBruno] = useState(false);
+
+  const handleClick = (familyMember) => {
+    // set the state to render the description of the family member who is clicked on
+    setMadrigal(familyMember);
+
+    if (familyMember.name === 'Bruno') {
+      setWeDontTalkAboutBruno(true);
+    }
+  } 
+
   return (
     <>
       <header>
@@ -10,13 +35,14 @@ function App() {
           height="300px;"
           alt={mirabelMadrigal.alt}
         />
-        <h1>{mirabelMadrigal.description}</h1>
+        <h1>{madrigal.description}</h1>
       </header>
       <main>
         <section>
+          {/* familyMadrigal array from familyMadrigal.js */}
           {familyMadrigal.map((member) => {
             return (
-              <div key={member.name}>
+              <div key={member.name} onClick={() => handleClick(member)} className={weDontTalkAboutBruno && member.name === 'Bruno' ? 'bruno' : null}>
                 <img
                   src={member.image}
                   alt={member.alt}
